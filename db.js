d936193720640148abfa6779e39f70b1e275936e
Aniket Thapa <aniketthapa04@gmail.com>
2024-12-22 00:25:49 +0530

    first commit

--- db.js
+++ db.js
@@ -1,13 +1,35 @@
 const mysql = require('mysql2');
 
+/*
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'aniket',
-  database: 'library_management',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  connectTimeout: 30000,
+});
+
+/*
+*/
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: process.env.DB_caCert,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 30000,
 });
 
 module.exports = pool;

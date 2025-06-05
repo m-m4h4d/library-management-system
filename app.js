797449798c23d9578e5664f21382eddc2974da08
Aniket Thapa <aniketthapa04@gmail.com>
2025-06-05 12:23:15 +0530

    updated

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
